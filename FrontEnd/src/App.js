14f88c84ec836b761b83bd046d043fcf2ddccf34
JWK750 <joshuawk@live.unc.edu>
2019-05-23 12:06:31 -0400

    cleared app file

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
+  /*  <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,7 +20,8 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </div> */
+    <Header />
   );
 }
 
