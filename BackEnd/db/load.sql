344e40ea8b3a689d46611e0e5b59091acc24aa00
Sam Chan <spc28@duke.edu>
2019-05-23 12:24:18 -0400

    Tables working, load.sql has 1 sample row for each table

--- BackEnd/db/load.sql
+++ BackEnd/db/load.sql
@@ -1,1 +1,3 @@
 insert into Person(id, name, email, password, phone, rating) values (0, 'a random guy', 'segar0@salon.com', 'paswword', '751-818-2357', 42.3);
+insert into Entry(id, personId, origin, destination, startTime, endTime, radiusMiles, type, comment) values (0, 0, 42.3333, 0.4443, '1999-01-08 04:05:06', '1999-01-08 07:05:06', 0.22, 'Uber', 'I need a ride');
+insert into Groups(id, group_members) values (0, 'a random guy');
