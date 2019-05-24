c3f0b1f5e5bd4d4b7bd1c09aadf8a74cf1ba21c4
Sam Chan <spc28@duke.edu>
2019-05-23 20:00:31 -0400

    Overwrote entry/id with merge, new push w/ entry/id included

--- BackEnd/flask/config.py
+++ BackEnd/flask/config.py
@@ -1,6 +1,6 @@
 #Change username to ensure PostgreSQL db is working
 #TODO: Deploy server, React app, and database to a cloud service such as AWS, GCP
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:abc123@0.0.0.0:5432/hitchride'
+SQLALCHEMY_DATABASE_URI = 'postgresql://samchan:dbpasswd@0.0.0.0:5432/hitchride'
 SQLALCHEMY_ECHO = True
 DEBUG = True
 SQLALCHEMY_TRACK_MODIFICATIONS = False
