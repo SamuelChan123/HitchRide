fa367e95727663f128dcca143422ad4aafb91fb3
EC2 Default User <ec2-user@ip-172-31-44-203.ec2.internal>
2019-05-24 02:14:25 +0000

    modify database creation

--- BackEnd/flask/models.py
+++ BackEnd/flask/models.py
@@ -9,7 +9,6 @@ class Person(db.Model):
     id = db.Column('id', db.Integer(), primary_key=True)
     name = db.Column('name', db.String(64))
     email = db.Column('email', db.String(64))
-    password = db.Column('password', db.String(64))
     phone = db.Column('phone', db.String(32))
     rating = db.Column('rating', db.Float())
 
@@ -22,7 +21,6 @@ class Entry(db.Model):
     origin = db.Column('origin',db.Float())
     destination = db.Column('destination', db.Float())
     startTime = db.Column('starttime', db.TIMESTAMP())
-    endTime = db.Column('endtime', db.TIMESTAMP())
     radiusMiles = db.Column('radiusmiles', db.Float())
     type = db.Column('type', db.String(32))
     comment = db.Column('comment', db.String(64))
